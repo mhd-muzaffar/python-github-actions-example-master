3ad652c05b5ef9b1638b99aa20fa59f7ddf8588c
muzaffar <mhd.muzaffar@gmail.com>
2022-10-14 12:06:01 +0530

    last coomit

--- src/app.py
+++ src/app.py
@@ -4,11 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-<<<<<<< HEAD
-    return "Hello,IT world!"
-=======
-    return "Hello,NSC123 world!"
->>>>>>> 20a03b6c1dd3c43e868d5a8fe5692b656714cf5f
+	return "Hello,NSC123 world!"
+
 
 
 if __name__ == "__main__":
